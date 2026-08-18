03838ed45eb814f02fa09a9e90449e6e0cd6e999
Abhishek <abhishek.kumar2@pharmeasy.in>
2026-08-18 15:17:04 +0530

    remove error dags

--- airflow_home/dags/dag_broken_example.py
+++ airflow_home/dags/dag_broken_example.py
@@ -1,27 +1,27 @@
-from datetime import datetime, timedelta
+# from datetime import datetime, timedelta
 
-from airflow import DAG
-from airflow.operators.python import PythonOperator
-
-
-def broken_task_function()  # <-- missing colon: this is a SyntaxError
-    print("This will never run")
+# from airflow import DAG
+# from airflow.operators.python import PythonOperator
 
 
-default_args = {
-    "owner": "data_platform_team",
-    "retries": 1,
-}
+# def broken_task_function()  # <-- missing colon: this is a SyntaxError
+#     print("This will never run")
 
-with DAG(
-    dag_id="dag_broken_example",
-    default_args=default_args,
-    schedule_interval="@daily",
-    start_date=datetime(2024, 1, 1),
-    catchup=False,
-) as dag:
 
-    broken_task = PythonOperator(
-        task_id="broken_task",
-        python_callable=broken_task_function,
-    )
+# default_args = {
+#     "owner": "data_platform_team",
+#     "retries": 1,
+# }
+
+# with DAG(
+#     dag_id="dag_broken_example",
+#     default_args=default_args,
+#     schedule_interval="@daily",
+#     start_date=datetime(2024, 1, 1),
+#     catchup=False,
+# ) as dag:
+
+#     broken_task = PythonOperator(
+#         task_id="broken_task",
+#         python_callable=broken_task_function,
+#     )
