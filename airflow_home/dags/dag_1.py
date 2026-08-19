61e3741c1f0f32260547d272ff6bf3b2b66ff12c
Abhishek <abhishek.kumar2@pharmeasy.in>
2026-08-19 11:09:09 +0530

    import check

--- airflow_home/dags/dag_1.py
+++ airflow_home/dags/dag_1.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timedelta
 
 from airflow import DAG
-from airflow.operators.python import PythonOperator
+from airflow.operators.python import PythonOperato
 
 
 def say_hello():
