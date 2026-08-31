fac84c242b921eb1f16844c6cbe14c59fa40808a
Abhishek <abhishek.kumar2@pharmeasy.in>
2026-08-31 16:24:18 +0530

    update code

--- example.sql
+++ example.sql
@@ -11,4 +11,4 @@ WHERE o.order_date >= '2024-01-01'
     AND o.order_amount > 100
 GROUP BY u.user_id, u.user_name, o.order_id, o.order_amount, o.order_date
 ORDER BY o.order_amount DESC
-LIMIT 50;
+LIMIT 50
